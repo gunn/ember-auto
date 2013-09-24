07f680f8485d7b3d65a7fdc681d46a80bbfd0d73
Arthur Gunn <arthur@gunn.co.nz>
2013-09-24 18:36:29 +1200

    Changes to make jshint happy.

--- packages/ember-auto/lib/main.js
+++ packages/ember-auto/lib/main.js
@@ -2,7 +2,7 @@ require("ember-auto/auto-computed-property");
 
 Ember.auto = function (func) {
   return autoComputed(func);
-}
+};
 
 require("ember-auto/version");
 require("ember-auto/computed-property-extensions");
--- packages/ember-auto/lib/version.js
+++ packages/ember-auto/lib/version.js
@@ -1,3 +1,5 @@
 Ember.auto.VERSION = "0.0.1a";
 
-Ember.libraries && Ember.libraries.register("Ember Auto", Ember.auto.VERSION);
+if (Ember.libraries) {
+  Ember.libraries.register("Ember Auto", Ember.auto.VERSION);
+}
