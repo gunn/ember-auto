c71a5f666e2ab112daf263fe45b5331782d2c12f
Arthur Gunn <arthur@gunn.co.nz>
2013-09-24 19:10:19 +1200

    Fixed version number.

--- packages/ember-auto/tests/version_test.js
+++ packages/ember-auto/tests/version_test.js
@@ -6,5 +6,5 @@ test("the version number is set", function() {
   expect(2);
 
   ok(Ember.auto.VERSION, "it's defined");
-  ok(Ember.auto.VERSION.match(/^\d+\.\d+\.\d+$/), "it's in standard versio  number form");
+  ok(Ember.auto.VERSION.match(/^\d+\.\d+\.\d+\w*$/), "it's in standard version number form");
 });
