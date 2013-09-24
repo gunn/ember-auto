eed76024cd43bba881b736541e4df63e521d035a
Arthur Gunn <arthur@gunn.co.nz>
2013-09-24 22:10:08 +1200

    Parse dependent keys from the function if they're not provided explicitly.

--- packages/ember-auto/lib/auto_computed_property.js
+++ packages/ember-auto/lib/auto_computed_property.js
@@ -1,6 +1,35 @@
+var argumentNamesFor = function(func) {
+  var FN_ARGS        = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+  var FN_ARG_SPLIT   = /,/;
+  var FN_ARG         = /^\s*(_?)(\S+?)\1\s*$/;
+  var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+
+  var argNames = [];
+
+  if (func.length) {
+    var fnText  = func.toString().replace(STRIP_COMMENTS, '');
+    var argDecl = fnText.match(FN_ARGS);
+    (argDecl[1].split(FN_ARG_SPLIT)).forEach(function(arg) {
+      arg.replace(FN_ARG, function(all, underscore, name){
+        argNames.push(name);
+      });
+    });
+  }
+
+  return argNames;
+};
+
 Ember.AutoProperty = function() {
   var cp = Ember.computed.apply(Ember, arguments);
   cp._isAutoProperty = true;
+
+  if (!cp._dependentKeys) {
+    var keys = argumentNamesFor(cp.func);
+
+    if (keys.length) {
+      cp._dependentKeys = keys;
+    }
+  }
 
   return cp;
 };
