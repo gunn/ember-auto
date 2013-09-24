9fd7a3950657f13e1b4900e82b4613bde136982e
Arthur Gunn <arthur@gunn.co.nz>
2013-09-24 19:59:15 +1200

    Add test for auto properties being computed properties.

--- packages/ember-auto/tests/auto_computed_property_test.js
+++ packages/ember-auto/tests/auto_computed_property_test.js
@@ -1,10 +1,5 @@
 module("Ember.auto");
 
-test("Ember.auto creates auto properties", function() {
-  var property = Ember.auto(function () {});
-  ok(property._isAutoProperty, "Ember.auto creates an auto property");
-});
-
 test("Regular computed properties are not auto", function() {
   expect(2);
 
@@ -13,4 +8,18 @@ test("Regular computed properties are not auto", function() {
 
   ok(!property1._isAutoProperty, "Ember.computed does not create an auto property");
   ok(!property2._isAutoProperty, "func.property() does not create an auto property");
+});
+
+test("Ember.auto creates auto properties", function() {
+  var property = Ember.auto(function () {});
+  ok(property._isAutoProperty, "Ember.auto creates an auto property");
+});
+
+test("auto properties are computed properties", function() {
+  expect(3);
+
+  var property = Ember.auto(function () {});
+  ok(property.cacheable, "an auto property is a computed property");
+  ok(property.volatile,  "an auto property is a computed property");
+  ok(property.readOnly,  "an auto property is a computed property");
 });
