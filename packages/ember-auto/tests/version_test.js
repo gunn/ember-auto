2cf82339dce31770cd25e5b3cd5e0cf41d5b8293
Arthur Gunn <arthur@gunn.co.nz>
2013-09-24 18:56:52 +1200

    Simplify and improve version number tests.

--- packages/ember-auto/tests/version_test.js
+++ packages/ember-auto/tests/version_test.js
@@ -5,8 +5,6 @@ module("Ember.auto.VERSION");
 test("the version number is set", function() {
   expect(2);
 
-  var type = typeof Ember.auto.VERSION;
-  equal(type, "string", "Ember.auto.VERSION should be a string");
-
-  ok(Ember.auto.VERSION.match(/^\d+\.\d+\.\d+/), "it's in standard versio  number form");
+  ok(Ember.auto.VERSION, "it's defined");
+  ok(Ember.auto.VERSION.match(/^\d+\.\d+\.\d+$/), "it's in standard versio  number form");
 });
