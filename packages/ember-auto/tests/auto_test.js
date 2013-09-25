6873feee029094adf33b8c07a6d236c4d828c485
Arthur Gunn <arthur@gunn.co.nz>
2013-09-25 23:15:46 +1200

    Added tests for auto passing arguments to functions.

--- packages/ember-auto/tests/auto_test.js
+++ packages/ember-auto/tests/auto_test.js
@@ -11,3 +11,33 @@ test("get dependent keys from the function unless provided explicitly", function
   deepEqual(p2._dependentKeys, ["a", "b", "c"]);
   deepEqual(p3._dependentKeys, ["X", "Y"]);
 });
+
+test("the dependent key properties are passed into the function", function() {
+  expect(1);
+
+  var Person = Ember.Object.extend({
+    full: Ember.auto(function(first, last) {
+      return first + " " + last;
+    })
+  });
+
+  var obj = Person.create({first: "Arthur", last: "Gunn"});
+  equal(obj.get("full"), "Arthur Gunn", "properties are passed as arguments");
+
+  obj.set("first", "Attila the");
+  equal(obj.get("full"), "Attila the Gunn", "changed properties are passed as arguments");
+});
+
+test("argument orders can be rearranged, skipped", function () {
+  var Numbers = Ember.Object.create({
+    a: 1, b: 2, c: 3, d: 4, e: 5, f: 6,
+
+    list: Ember.auto(function (a, b, c, d, e, f) {
+      return [a, b, c, d, e, f];
+    })
+  });
+
+  var num = Numbers.create();
+
+  deepEqual(num.get("list"), [1,2,3,4,5,6]);
+});
