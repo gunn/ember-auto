f5ee2b81c54d5b842054f38e496d62b7b0fa3465
Arthur Gunn <arthur@gunn.co.nz>
2013-09-26 08:47:53 +1200

    Fix tests, use Ember.get.

--- packages/ember-auto/tests/auto_test.js
+++ packages/ember-auto/tests/auto_test.js
@@ -1,5 +1,8 @@
 module("auto properties");
 
+var get = Ember.get;
+
+
 test("get dependent keys from the function unless provided explicitly", function() {
   expect(3);
 
@@ -13,7 +16,7 @@ test("get dependent keys from the function unless provided explicitly", function
 });
 
 test("the dependent key properties are passed into the function", function() {
-  expect(1);
+  expect(2);
 
   var Person = Ember.Object.extend({
     full: Ember.auto(function(first, last) {
@@ -22,14 +25,14 @@ test("the dependent key properties are passed into the function", function() {
   });
 
   var obj = Person.create({first: "Arthur", last: "Gunn"});
-  equal(obj.get("full"), "Arthur Gunn", "properties are passed as arguments");
+  equal(get(obj, "full"), "Arthur Gunn", "properties are passed as arguments");
 
   obj.set("first", "Attila the");
-  equal(obj.get("full"), "Attila the Gunn", "changed properties are passed as arguments");
+  equal(get(obj, "full"), "Attila the Gunn", "changed properties are passed as arguments");
 });
 
 test("argument orders can be rearranged, skipped", function () {
-  var Numbers = Ember.Object.create({
+  var Numbers = Ember.Object.extend({
     a: 1, b: 2, c: 3, d: 4, e: 5, f: 6,
 
     list: Ember.auto(function (a, b, c, d, e, f) {
@@ -39,5 +42,5 @@ test("argument orders can be rearranged, skipped", function () {
 
   var num = Numbers.create();
 
-  deepEqual(num.get("list"), [1,2,3,4,5,6]);
+  deepEqual(get(num, "list"), [1, 2, 3, 4, 5, 6]);
 });
