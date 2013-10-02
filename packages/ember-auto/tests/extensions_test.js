d571b16522a88a05d65164f94e6998437cefd9ff
Arthur Gunn <arthur@gunn.co.nz>
2013-10-02 13:28:00 +1300

    Add tests for Function extenstion.

--- packages/ember-auto/tests/extensions_test.js
+++ packages/ember-auto/tests/extensions_test.js
@@ -30,3 +30,27 @@ test("calling auto on a computed property creates an auto property", function() 
   equal(get(obj, "full1"), "Arthur Gunn", "properties are injected as arguments");
   equal(get(obj, "full2"), "Arthur Gunn", "properties are injected as arguments");
 });
+
+test("dependent keys can be specified", function () {
+  var Numbers, nums;
+  expect(3);
+
+  Numbers = Ember.Object.extend({
+    a: 1, b: 2, c: 3, d: 4,
+
+    list1: function (a, b, c, d) {
+      return [].slice.call(arguments);
+    }.auto("a", "b", "c", "d"),
+    list2: function (a, b, c, d) {
+      return [].slice.call(arguments);
+    }.auto("a", "b"),
+    list3: function (a, b, c, d) {
+      return [].slice.call(arguments);
+    }.auto("d", "b", "c")
+  });
+  nums = Numbers.create();
+
+  deepEqual(get(nums, "list1"), [1, 2, 3, 4]);
+  deepEqual(get(nums, "list2"), [1, 2, undefined, undefined], "arguments that don't map to dependent keys are injected as undefined");
+  deepEqual(get(nums, "list3"), [undefined, 2, 3, 4], "works with any order of dependent keys");
+});
