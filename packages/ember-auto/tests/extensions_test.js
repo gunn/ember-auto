2646e274b21e8e13ab2c40f0a406c2c302557a38
Arthur Gunn <arthur@gunn.co.nz>
2013-10-02 14:35:09 +1300

    Add tests for extensions.

--- packages/ember-auto/tests/extensions_test.js
+++ packages/ember-auto/tests/extensions_test.js
@@ -50,3 +50,25 @@ test("dependent keys can be specified", function () {
   deepEqual(get(nums, "list2"), [1, 2, undefined, undefined], "arguments that don't map to dependent keys are injected as undefined");
   deepEqual(get(nums, "list3"), [undefined, 2, 3, 4], "works with any order of dependent keys");
 });
+
+test(".auto can be used to specify keys on computed properties, .property on auto properties", function () {
+  expect(4);
+
+  var list = function (a, b, c, d) {
+    return [].slice.call(arguments);
+  };
+
+  var nums = Ember.Object.extend({
+    a: 1, b: 2, c: 3, d: 4,
+
+    list1: Ember.computed(list).auto("c", "b"),
+    list2: Ember.auto(list).property("c", "b"),
+    list3: list.property().auto("c", "b"),
+    list4: list.auto().property("c", "b")
+  }).create();
+
+  deepEqual(get(nums, "list1"), [undefined, 2, 3, undefined], ".auto specifying keys on a computed property");
+  deepEqual(get(nums, "list2"), [undefined, 2, 3, undefined], ".property specifying keys on an auto property");
+  deepEqual(get(nums, "list3"), [undefined, 2, 3, undefined], ".auto specifying keys after a .property");
+  deepEqual(get(nums, "list4"), [undefined, 2, 3, undefined], ".property specifying keys after a .auto");
+});
